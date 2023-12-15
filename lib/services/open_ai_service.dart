25d94f1ef682bacf5260daded41d9275d5f24fd9
Alonso Maya <alonsorubmaya@gmail.com>
2023-12-14 18:58:29 -0600

    Use version 4

--- lib/services/open_ai_service.dart
+++ lib/services/open_ai_service.dart
@@ -14,7 +14,7 @@ class OpenAIService {
       role: OpenAIChatMessageRole.assistant,
     );
     final solution = await OpenAI.instance.chat.create(
-      model: 'gpt-3.5-turbo-1106',
+      model: 'gpt-4-1106-preview',
       responseFormat: {"type": "json_object"},
       messages: [
         definitionSystem,
@@ -28,6 +28,7 @@ class OpenAIService {
         )
       ],
       temperature: 0.8,
+      frequencyPenalty: 0,
     );
 
     return solution.choices.first.message.content?.first.text ?? '';
